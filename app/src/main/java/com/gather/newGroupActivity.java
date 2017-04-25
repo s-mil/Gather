1cfe440a5bc056be80ee903dc4c78c5e0e2776eb
Sam <sithpow@gmail.com>
2017-04-25 17:27:47 -0500

    finished activity_new_group.xml

--- app/src/main/java/com/gather/newGroupActivity.java
+++ app/src/main/java/com/gather/newGroupActivity.java
@@ -1,4 +1,13 @@
 package com.gather;
-public class newGroupActivity {
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_group);
+    }
 }
