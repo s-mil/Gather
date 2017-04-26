5008f8deca5e020caa05e22e3bffcf9b555ea2eb
jam-millsap <jam.millsap@gmail.com>
2017-04-25 21:30:12 -0500

    Finally fixed the profile and changing skill levels removed booleans cause fuck that shit

--- app/src/main/java/com/gather/User.java
+++ app/src/main/java/com/gather/User.java
@@ -3,7 +3,6 @@ import android.view.Window;
 
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-
 public class User {
     public int ArtLevel =   0;
     public int DesignLevel =   0;
@@ -18,20 +17,6 @@ public class User {
     public int IOSLevel =  0;
     public int CPPLevel =  0;
     public int HTMLLevel =  0;
-
-    public boolean isArt = false;
-    public boolean isDesign = false;
-    public boolean isLeadership = false;
-    public boolean isJava = false;
-    public boolean isPython = false;
-    public boolean isCSharp = false;
-    public boolean isWindows = false;
-    public boolean isLinux = false;
-    public boolean isOSX = false;
-    public boolean isAndroid = false;
-    public boolean isIOS = false;
-    public boolean isCPP = false;
-    public boolean isHTML = false;
     User(){
 
     }
@@ -49,36 +34,6 @@ public class User {
         this.IOSLevel = IOSlvl;
         this.CPPLevel = CPPlvl;
         this.HTMLLevel = HTMPlvl;
-    }
-    public ArrayList WhatIs() {
-        ArrayList<String> list = new ArrayList<>();
-        if (isArt)
-            list.add("Art");
-        if (isDesign)
-            list.add("Design");
-        if (isLeadership)
-            list.add("Leadership");
-        if (isJava)
-            list.add("Java");
-        if (isPython)
-            list.add("Python");
-        if (isCSharp)
-            list.add("CSharp");
-        if (isWindows)
-            list.add("Windows");
-        if (isLinux)
-            list.add("Linux");
-        if (isOSX)
-            list.add("OSX");
-        if (isAndroid)
-            list.add("Android");
-        if (isIOS)
-            list.add("IOS");
-        if (isCPP)
-            list.add("CPP");
-        if (isHTML)
-            list.add("HTML");
-        return list;
     }
     public ArrayList getLevels() {
         ArrayList<Integer> list = new ArrayList<>();
--- app/src/main/java/com/gather/myProfileActivity.java
+++ app/src/main/java/com/gather/myProfileActivity.java
@@ -2,6 +2,12 @@ package com.gather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -10,7 +16,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 
 public class myProfileActivity extends AppCompatActivity {
@@ -18,6 +23,42 @@ public class myProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+        final String[] vals = new String [] {"Change your skill level","0","1","2","3","4","5","6","7","8","9","10"};
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, vals);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinnerchangeArt ,spinnerchangeDesign ,spinnerchangeLeadership ,spinnerchangeJava ,spinnerchangePython ,spinnerchangeCSharp ,spinnerchangeWindows ,spinnerchangeLinux ,spinnerchangeOSX ,spinnerchangeAndroid ,spinnerchangeIOS ,spinnerchangeCPP ,spinnerchangeHTML;
+        spinnerchangeArt = (Spinner) findViewById(R.id.spinner_Art);
+        spinnerchangeDesign = (Spinner) findViewById(R.id.spinner_Design);
+        spinnerchangeLeadership = (Spinner) findViewById(R.id.spinner_Leadership);
+        spinnerchangeJava = (Spinner) findViewById(R.id.spinner_Java);
+        spinnerchangePython = (Spinner) findViewById(R.id.spinner_Python);
+        spinnerchangeCSharp = (Spinner) findViewById(R.id.spinner_CSharp);
+        spinnerchangeWindows = (Spinner) findViewById(R.id.spinner_Windows);
+        spinnerchangeLinux = (Spinner) findViewById(R.id.spinner_Windows);
+        spinnerchangeOSX = (Spinner) findViewById(R.id.spinner_OSX);
+        spinnerchangeAndroid = (Spinner) findViewById(R.id.spinner_Android);
+        spinnerchangeIOS = (Spinner) findViewById(R.id.spinner_IOS);
+        spinnerchangeCPP = (Spinner) findViewById(R.id.spinner_CPP);
+        spinnerchangeHTML = (Spinner) findViewById(R.id.spinner_HTML);
+
+        spinnerchangeArt.setAdapter(adapter);
+        spinnerchangeArt.setSelection(0);
+        spinnerchangeDesign.setAdapter(adapter);spinnerchangeDesign.setSelection(0);
+        spinnerchangeLeadership.setAdapter(adapter);spinnerchangeLeadership.setSelection(0);
+        spinnerchangeJava.setAdapter(adapter);spinnerchangeJava.setSelection(0);
+        spinnerchangePython.setAdapter(adapter);spinnerchangePython.setSelection(0);
+        spinnerchangeCSharp.setAdapter(adapter);spinnerchangeCSharp.setSelection(0);
+        spinnerchangeWindows.setAdapter(adapter);spinnerchangeWindows.setSelection(0);
+        spinnerchangeLinux.setAdapter(adapter);spinnerchangeLinux.setSelection(0);
+        spinnerchangeOSX.setAdapter(adapter);spinnerchangeOSX.setSelection(0);
+        spinnerchangeAndroid.setAdapter(adapter);spinnerchangeAndroid.setSelection(0);
+        spinnerchangeIOS.setAdapter(adapter);spinnerchangeIOS.setSelection(0);
+        spinnerchangeCPP.setAdapter(adapter);spinnerchangeCPP.setSelection(0);
+        spinnerchangeHTML.setAdapter(adapter);spinnerchangeHTML.setSelection(0);
+
+
+
 
         final TextView Art, Design, Leadership, Java, Python, CSharp, Windows, Linux, OSX, Android, IOS, CPP, HTML;
         Art = (TextView) findViewById(R.id.textArt);
@@ -34,11 +75,10 @@ public class myProfileActivity extends AppCompatActivity {
         CPP = (TextView) findViewById(R.id.textCPP);
         HTML = (TextView) findViewById(R.id.textHTML);
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
         String uid = auth.getCurrentUser().getUid();
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
         DatabaseReference userInfo = ref.child("users").child(uid);
-        userInfo.addListenerForSingleValueEvent(new ValueEventListener() {
+        userInfo.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 User user = dataSnapshot.getValue(User.class);
@@ -62,5 +102,153 @@ public class myProfileActivity extends AppCompatActivity {
 
             }
         });
+        spinnerchangeArt.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"ArtLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerchangeDesign.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"DesignLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerchangeLeadership.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"LeadershipLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerchangeJava.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"JavaLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerchangePython.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"PythonLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerchangeCSharp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"CSharpLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerchangeWindows.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"WindowsLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerchangeLinux.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"LinuxLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerchangeOSX.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"OSXLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerchangeAndroid.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"AndroidLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerchangeIOS.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"IOSLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerchangeCPP.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"CPPLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinnerchangeHTML.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    changeData(position-1,"HTMLLevel");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+    final FirebaseAuth auth = FirebaseAuth.getInstance();
+    public void changeData (int val, String str) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("users").child(auth.getCurrentUser().getUid()).child(str);
+        ref.setValue(val);
     }
 }
