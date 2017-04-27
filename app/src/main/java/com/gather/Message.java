3e099f6365354ce419468445683a2a61e7112a7d
Sweta Ghimire <sghimi3@lsu.edu>
2017-04-26 20:44:04 -0500

    Improve group chat scrolling and UI

--- app/src/main/java/com/gather/Message.java
+++ app/src/main/java/com/gather/Message.java
@@ -4,24 +4,11 @@ public class Message {
     public String sender;
     public String message;
 
-//  public String chatPartnerId() {
-//      FirebaseAuth auth = FirebaseAuth.getInstance();
-//      if (auth.getCurrentUser().getUid() != null) {
-//          if (fromId == toId)
-//              return toId;
-//          else
-//              return fromId;
-//      }
-//  }
-
     public Message(String sender, String message) {
         this.sender = sender;
         this.message = message;
     }
 
-    public String getSender() {
-        return "SENDER";
-    }
     public Message(){
 
     }
